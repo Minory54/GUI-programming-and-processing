b80ff2ca43a0e479b7888f8afa678dd0897ccee6
Minory54 <72006176+Minory54@users.noreply.github.com>
2024-02-15 10:21:37 +0700

    Added solution N4

--- Laba_2/N4/MainWindow.xaml.cs
+++ Laba_2/N4/MainWindow.xaml.cs
@@ -25,7 +25,7 @@ namespace N4
         {
             InitializeComponent();
 
-            int[] year = {2021, 2022, 2023, 2024};           
+            int[] year = {2019, 2020, 2021, 2022, 2023};
             string[] month = { "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь" };
 
             for (int j = 0; j < year.Length; j++)
@@ -39,110 +39,138 @@ namespace N4
             }
         }
 
-        int fillingCmbDays (int year, int indexMonth)
+        void fillingCmbDays() 
         {
-            int daysCount = 0;
+            int year = Convert.ToInt32(cmb_year.SelectedValue);
+            int indexMonth = cmb_month.SelectedIndex + 1;
+            cmb_day.Items.Clear();
+
+
+            if (cmb_year.SelectedItem != null & cmb_month.SelectedItem != null)
+            {
+                fillingdaysCountInMonth(daysCountInMonth(year, indexMonth));
+                cmb_day.IsEnabled = true;
+            }
+        }
+
+        int daysCountInMonth(int selectedYear, int indexMonth)
+        {
+            int daysCountInMonth = 0;
 
             switch (indexMonth)
             {
-                case 0: //Январь
-                    daysCount = 31; 
+                case 1: //Январь
+                    daysCountInMonth = 31; 
                     break;
-                case 1: //Февраль
-                    if (year % 4 == 0)
+                case 2: //Февраль
+                    if (selectedYear % 4 == 0)
                     {
-                        daysCount = 29;
+                        daysCountInMonth = 29;
                     } else
                     {
-                        daysCount = 28;
+                        daysCountInMonth = 28;
                     }                    
                     break;
-                case 2: //Март
-                    daysCount = 31; 
+                case 3: //Март
+                    daysCountInMonth = 31; 
                     break;
-                case 3: //Апрель
-                    daysCount = 30; 
+                case 4: //Апрель
+                    daysCountInMonth = 30; 
                     break;
-                case 4: //Май
-                    daysCount = 31;
+                case 5: //Май
+                    daysCountInMonth = 31;
                     break;
-                case 5: //Июнь
-                    daysCount = 30;
+                case 6: //Июнь
+                    daysCountInMonth = 30;
                     break;
-                case 6: //Июль
-                    daysCount = 31;
+                case 7: //Июль
+                    daysCountInMonth = 31;
                     break;
-                case 7: //Август
-                    daysCount = 31;
+                case 8: //Август
+                    daysCountInMonth = 31;
                     break;
-                case 8: //Сентябрь
-                    daysCount = 30;
+                case 9: //Сентябрь
+                    daysCountInMonth = 30;
                     break;
-                case 9: //Октябрь
-                    daysCount = 31;
+                case 10: //Октябрь
+                    daysCountInMonth = 31;
                     break;
-                case 10: //Ноябрь
-                    daysCount = 30;
+                case 11: //Ноябрь
+                    daysCountInMonth = 30;
                     break;
-                case 11: //Декабрь
-                    daysCount = 31;
+                case 12: //Декабрь
+                    daysCountInMonth = 31;
                     break;
             }
-
-            fillingDaysCount (daysCount);
-            return daysCount;
+            return daysCountInMonth;
         }
 
-        void fillingDaysCount( int daysCount)
+        void fillingdaysCountInMonth(int daysCountInMonth)
         {
-            for (int i = 1; i <= daysCount; i++)
+            for (int i = 1; i <= daysCountInMonth; i++)
             {
                 cmb_day.Items.Add(i);
             }
         }
 
-        private void cmb_day_DropDownOpened(object sender, EventArgs e)
+        void pastTime ()
         {
-            int year = Convert.ToInt32(cmb_year.SelectedValue);
-            int indexMonth = cmb_month.SelectedIndex;
-            cmb_day.Items.Clear();
+            int selectedYear = Convert.ToInt32(cmb_year.SelectedValue);
+            int selectedIndexMonth = cmb_month.SelectedIndex + 1;
+            int selectedDay = Convert.ToInt32(cmb_day.SelectedValue);
 
 
-            if (cmb_year.SelectedItem != null & cmb_month.SelectedItem != null) 
+            //int dayPassed = Math.Abs(DateTime.Now.Day - day);          
+            //int monthPassed = Math.Abs(DateTime.Now.Month - indexMonth;
+            //int yearPassed = DateTime.Now.Year - year;
+
+            int dayPassed = 0;
+            int monthPassed = 0;
+            int yearPassed = 0;
+
+
+            if (DateTime.Now.Day - selectedDay < 0)
             {
-                fillingCmbDays(year, indexMonth);
+                dayPassed = daysCountInMonth(selectedYear, selectedIndexMonth) + DateTime.Now.Day - selectedDay;
+                monthPassed = DateTime.Now.Month - selectedIndexMonth - 1;
+            }
+            else
+            {
+                dayPassed = DateTime.Now.Day - selectedDay;
+                monthPassed = DateTime.Now.Month - selectedIndexMonth;
+            }
+
+            if (DateTime.Now.Month - selectedIndexMonth < 0 || daysCountInMonth(selectedYear, monthPassed) + monthPassed < 0)
+            {
+                monthPassed = 12 + DateTime.Now.Month - selectedIndexMonth;
+                yearPassed = DateTime.Now.Year - selectedYear - 1;
+            }
+            else
+            {
+                monthPassed = DateTime.Now.Month - selectedIndexMonth;
+                yearPassed = DateTime.Now.Year - selectedYear;
+            }
+
+
+            if (cmb_year.SelectedItem != null & cmb_month.SelectedItem != null & cmb_day.SelectedItem != null)
+            {
+                MessageBox.Show($"Прошло {yearPassed} года, {monthPassed} месяцев и {dayPassed} дней");
             }
         }
 
         private void cmb_day_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            int year = Convert.ToInt32(cmb_year.SelectedValue);
-            int indexMonth = cmb_month.SelectedIndex;
-            int day = Convert.ToInt32(cmb_day.SelectedValue);
-
-            int yearPassed = DateTime.Now.Year - year;
-            int monthPassed = DateTime.Now.Month - indexMonth-1;
-            int dayPassed = DateTime.Now.Day - day;
-
-            if (yearPassed > 0 )
-
-            if (cmb_year.SelectedItem != null & cmb_month.SelectedItem != null & cmb_month.SelectedItem != null)
-            {
-                MessageBox.Show($"Прошло {yearPassed} года, {monthPassed} месяцев и {dayPassed} дней");
-            }
-            
+            pastTime();
         }
 
         private void cmb_year_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            fillingCmbDays();
         }
 
         private void cmb_month_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            fillingCmbDays();
         }
-
-        
     }
 }
