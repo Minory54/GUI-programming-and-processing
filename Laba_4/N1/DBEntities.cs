7c2ae4cc861cb257c20ee5deabfd8d4f83340d21
Minory <72006176+Minory54@users.noreply.github.com>
2024-03-27 17:36:19 +0700

    added delete journal information (broken)

--- Laba_4/N1/DBEntities.cs
+++ Laba_4/N1/DBEntities.cs
@@ -10,7 +10,6 @@ namespace N1
     internal class DBEntities : DbContext //Для подключения к базе данных (DBEntities бедется из Appconfig)
     {
         public DBEntities() : base("DefaultConnection") { }
-
         public DbSet<Student> Students { get; set; }
         public DbSet<Journal> Journals { get; set; }
     }
--- Laba_4/N1/MainWindow.xaml.cs
+++ Laba_4/N1/MainWindow.xaml.cs
@@ -34,18 +34,12 @@ namespace N1
         { 
             try 
             {
-                //var students = from s in db.Students
-                //               select new
-                //               { id = s.id, FirstName = s.FirstName};
-
-                //dg_Jurnal.ItemsSource = students.ToList();
-
-                var jurnal = from j in db.Journals
+                var journal = from j in db.Journals
                              join stud in db.Students on j.id_Student equals stud.id
                              select new
                                { id = j.id, StudentFIO = stud.FirstName + " " + stud.Surname + " " + stud.LastName, Physics = j.Physics, Math = j.Math};
 
-                dg_Jurnal.ItemsSource = jurnal.ToList();
+                dg_Journal.ItemsSource = journal.ToList();
 
             }
             catch (Exception ex)
@@ -54,13 +48,47 @@ namespace N1
             }
         }
 
-        private void dg_Jurnal_Loaded(object sender, RoutedEventArgs e)
+        private void dg_Journal_Loaded(object sender, RoutedEventArgs e)
         {
-            
-           
-
             UpdateTable();
         }
 
+        private void btn_del_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Вы действительно хотите удалить информацию о студенте?", "Подтверждение удаления", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            { 
+                try 
+                {                   
+                    var ci = new DataGridCellInfo(dg_Journal.SelectedItem, dg_Journal.Columns[0]);
+                    var id_journal = Convert.ToInt32((ci.Column.GetCellContent(ci.Item) as TextBlock).Text);
+
+                    Journal journal = db.Journals.Where(j => j.id == id_journal).FirstOrDefault();
+                    db.Journals.Remove(journal);
+                    db.SaveChanges();
+                    UpdateTable();
+
+                    MessageBox.Show("Удаление прошло успешно!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+        }
+
+        private void btn_add_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_edit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_Update_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateTable();
+        }
     }
 }
