df0b7ab930704276bca033849ea2d94b824843ab
KamysDev <work.kamysdev@gmail.com>
2024-05-22 18:39:53 +0700

    Created base of application

--- Laba_8/Sapper/MainWindow.xaml.cs
+++ Laba_8/Sapper/MainWindow.xaml.cs
@@ -20,9 +20,71 @@ namespace Sapper
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly int ButtonSize = 25;
+
         public MainWindow()
         {
-            InitializeComponent();
+            InitializeGame();
         }
+
+        private void InitializeGame()
+        {
+            InitializeComponent();
+
+            ///
+            /// Заполнение грида ячейками по заданным параметрам
+            ///
+
+            MainFrame.Rows = 10;
+            MainFrame.Columns = 10;
+
+            MainFrame.Height = MainFrame.Rows * (ButtonSize + 4);
+            MainFrame.Width = MainFrame.Columns * (ButtonSize + 4);
+
+            MainFrame.Margin = new Thickness(5);
+
+            ///
+            /// Делаем кнопочки во всех ячейках
+            ///
+
+            for (int i = 0; i < MainFrame.Rows * MainFrame.Columns; i++)
+            {
+                Button Btn = new Button();
+
+                Btn.Tag = i;
+                Btn.Width = Btn.Height = ButtonSize;
+                Btn.Content = " ";
+                Btn.Margin = new Thickness(2);
+                Btn.PreviewMouseDown += Btn_OnClick;
+
+                MainFrame.Children.Add(Btn);
+            }
+        }
+
+        /// 
+        /// AppLogic part. Init all events, prop and etc.
+        /// 
+
+        private void Btn_OnClick(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+
+                return;
+            }
+            else if (e.RightButton == MouseButtonState.Pressed)
+            {
+
+                return;
+            }
+            else if (e.MiddleButton == MouseButtonState.Pressed)
+            {
+
+                return;
+            }
+        }
+
+
+
     }
 }
