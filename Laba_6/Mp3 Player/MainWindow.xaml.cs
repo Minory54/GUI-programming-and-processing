ec9c2c4d2b044533293b0d5cdf622909b5a2d826
Minory <72006176+Minory54@users.noreply.github.com>
2024-05-23 00:16:40 +0700

    end laba 6

--- Laba_6/Mp3 Player/MainWindow.xaml.cs
+++ Laba_6/Mp3 Player/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,7 +13,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Mp3_Player
 {
@@ -20,9 +22,133 @@ namespace Mp3_Player
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        Dictionary<string, string> playlist = new Dictionary<string, string>();
+
+        DispatcherTimer Timer = new DispatcherTimer();
+        MediaPlayer player = new MediaPlayer();
+        Random rand = new Random();
+        int index = -1;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            sl_player.Value = player.Position.TotalSeconds;
+            lb_timer.Content = player.Position.ToString(@"mm\:ss");
+            player.MediaEnded += Player_MediaEnded;
+        }
+
+        private void Player_MediaOpened(object sender, EventArgs e)
+        {
+            lb_timerTotal.Content = player.NaturalDuration.TimeSpan.ToString(@"mm\:ss");
+            sl_player.Maximum = player.NaturalDuration.TimeSpan.TotalSeconds;
+            Timer.Start();
+        }
+
+        private void Player_MediaEnded(object sender, EventArgs e)
+        {
+            player.Stop();
+            index += 1;
+            if (index >= lb_listMusic.Items.Count) index = 0;
+            lb_listMusic.SelectedIndex = index;
+            lb_name.Content = lb_listMusic.SelectedItem.ToString();
+            player.Open(new Uri(playlist[lb_listMusic.SelectedItem.ToString()], UriKind.Relative));
+            player.MediaOpened += Player_MediaOpened;
+            player.Play();
+        }
+
+        private void mi_addFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Title = "Выбор медиа";
+            dlg.Filter = "mp3 files (*.mp3)|*.mp3";
+            dlg.Multiselect = true;
+            if (dlg.ShowDialog() == true)
+            {
+                foreach (String file in dlg.FileNames)
+                {
+                    lb_listMusic.Items.Add(Path.GetFileNameWithoutExtension(file));
+                    playlist[Path.GetFileNameWithoutExtension(file)] = file;
+                }
+            }
+        }
+
+        private void mi_delFile_Click(object sender, RoutedEventArgs e)
+        {
+            if (lb_listMusic.SelectedItem != null)
+            {
+                playlist.Remove(lb_listMusic.SelectedItem.ToString());
+                lb_listMusic.Items.RemoveAt(lb_listMusic.SelectedIndex);
+            }
+        }
+
+        private void btn_play_Click(object sender, RoutedEventArgs e)
+        {
+            try 
+            { 
+                player.Play();
+                sl_player.Maximum = player.NaturalDuration.TimeSpan.TotalSeconds;
+                Timer.Interval = TimeSpan.FromMilliseconds(100);
+                Timer.Tick += dispatcherTimer_Tick;
+            } 
+            catch { }
+
+        }
+
+        private void btn_pause_Click(object sender, RoutedEventArgs e)
+        {
+            player.Pause();
+        }
+
+        private void btn_stop_Click(object sender, RoutedEventArgs e)
+        {
+            player.Stop();
+        }
+
+        private void btn_rand_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (lb_listMusic.Items.Count != 0)
+            {
+                player.Stop();
+
+                index = rand.Next(0, lb_listMusic.Items.Count);
+                while (index == lb_listMusic.SelectedIndex)
+                {
+                    index = rand.Next(0, lb_listMusic.Items.Count);
+                }
+                lb_listMusic.SelectedIndex = index;
+                lb_name.Content = lb_listMusic.SelectedValue.ToString();
+                player.Open(new Uri(playlist[lb_listMusic.SelectedItem.ToString()], UriKind.Relative));
+                player.MediaOpened += Player_MediaOpened;
+            }
+
+        }
+
+        private void sl_volume_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            player.Volume = sl_volume.Value;
+        }
+
+        private void lb_listMusic_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (lb_listMusic.SelectedItem != null)
+            {
+                lb_name.Content = lb_listMusic.SelectedItem.ToString();
+                player.Stop();
+                player.Open(new Uri(playlist[lb_listMusic.SelectedItem.ToString()], UriKind.Relative));
+                player.MediaOpened += Player_MediaOpened;
+                index = lb_listMusic.SelectedIndex;
+            }
+        }
+
+        private void sl_player_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            player.Position = TimeSpan.FromSeconds(sl_player.Value);
         }
     }
 }
---
+++ Laba_6/Mp4 Player/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Mp4_Player
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer Timer = new DispatcherTimer();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            sl_player.Value = player.Position.TotalSeconds;
+            lb_timer.Content = player.Position.ToString(@"mm\:ss");
+            player.MediaEnded += Player_MediaEnded;
+        }
+
+        private void Player_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            player.Stop();
+            Timer.Stop();
+        }
+
+        private void mi_openFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Title = "Выбор медиа";
+            dlg.Filter = "video files (*.mp4)|*.mp4";
+            if (dlg.ShowDialog() == true)
+            {
+                player.Source = new Uri(dlg.FileName, UriKind.Relative);
+                player.Volume = sl_volume.Value;
+                player.MediaOpened += Player_MediaOpened;
+            }
+        }
+
+        private void Player_MediaOpened(object sender, RoutedEventArgs e)
+        {
+            sl_player.Maximum = player.NaturalDuration.TimeSpan.TotalSeconds;
+            lb_timerTotal.Content = player.NaturalDuration.TimeSpan.ToString(@"mm\:ss");
+            Timer.Interval = TimeSpan.FromMilliseconds(100);
+        }
+
+        private void btn_play_Click(object sender, RoutedEventArgs e)
+        {
+            player.Play();
+            sl_player.Value = player.Position.TotalSeconds;
+            Timer.Tick += dispatcherTimer_Tick;
+            Timer.Start();
+        }
+
+        private void btn_pause_Click(object sender, RoutedEventArgs e)
+        {
+            player.Pause();
+        }
+
+        private void btn_stop_Click(object sender, RoutedEventArgs e)
+        {
+            player.Stop();
+        }
+
+        private void sl_volume_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            player.Volume = sl_volume.Value;
+        }
+
+        private void sl_player_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            player.Position = TimeSpan.FromSeconds(sl_player.Value);
+        }
+    }
+}
